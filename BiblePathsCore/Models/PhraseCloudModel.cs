ea7a6e03d08b5997994f218cd870642532309736
Linden Goffar <Linden@outlook.com>
2022-01-14 17:58:11 -0800

    Integrated Search Functionality

---
+++ BiblePathsCore/Models/PhraseCloudModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace BiblePathsCore.Models
+{
+    public class PhraseCloudModel
+    {
+        public int NumWords { get; set; }
+        public string Input { get; set; }
+        public List<Phrase> FoundPhrases { get; set; }
+
+        public PhraseCloudModel(string input)
+        {
+            Input = input;
+        }
+
+
+    }
+
+    public class Phrase
+    {
+        public string Text { get; set; }
+        public int Count { get; set; }
+
+        public Phrase(string text, int count)
+        {
+            Text = text;
+            Count = count;
+        }
+    }
+}
