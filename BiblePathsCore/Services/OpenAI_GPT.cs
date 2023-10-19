2e18f6306e521d3e1beeae464bb27258052f93a4
Linden Goffar <Linden@outlook.com>
2023-10-19 16:42:11 -0700

    Updated Identity component to address issue

---
+++ BiblePathsCore/Services/OpenAI_GPT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BiblePathsCore.Services
+{
+    public interface IOpenAIResponder
+    {
+        string GetOpenAIResponseAsync(string key);
+    }
+
+    public class OpenAIResponder : IOpenAIResponder
+
+    {
+        private readonly IConfiguration _configuration;
+        public OpenAIResponder(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetOpenAIResponseAsync(string key)
+        {
+            return _configuration[key];
+        }
+    }
+}
+
