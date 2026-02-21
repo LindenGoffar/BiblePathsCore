0c653282a2a10a880eedbd276a2c00e2a0727b7c
Linden Goffar <Linden@Outlook.com>
2026-02-20 18:01:16 -0800

    All upgrades and new Blazor complete

--- BiblePathsCore/Program.cs
+++ BiblePathsCore/Program.cs
@@ -1,35 +1,112 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Azure.Identity;
+using BiblePathsCore.Data;
+using BiblePathsCore.Hubs;
+using BiblePathsCore.Models;
+using BiblePathsCore.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace BiblePathsCore
+var builder = WebApplication.CreateBuilder(args);
+
+// Optional: Azure Key Vault
+// var keyVaultEndpoint = new Uri(builder.Configuration["VaultUri"] ?? string.Empty);
+// builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AuthConnection")));
+
+builder.Services.AddDbContext<BiblePathsCoreDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                    .ConfigureAppConfiguration((context, config) =>
-                    {
-                    //var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-                    //var keyVaultEndpoint = new Uri("https://biblepathscorevault.vault.azure.net/");
-                    //config.AddAzureKeyVault(
-                    //keyVaultEndpoint,
-                    //new DefaultAzureCredential());
-                    })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.ReturnUrlParameter = "ReturnUrl";
+});
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
+
+builder.Services.AddAuthentication()
+    .AddGoogle(googleOptions =>
+    {
+        IConfigurationSection googleAuthNSection =
+            builder.Configuration.GetSection("Authentication:Google");
+
+        googleOptions.ClientId = googleAuthNSection["ClientId"];
+        googleOptions.ClientSecret = googleAuthNSection["ClientSecret"];
+    })
+    .AddMicrosoftAccount(microsoftOptions =>
+    {
+        microsoftOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"];
+        microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+    });
+
+builder.Services.AddRazorPages()
+    .AddRazorPagesOptions(options =>
+    {
+        options.Conventions.AddPageRoute("/CommentedPaths/Path", "/CommentedPaths/{name}");
+        options.Conventions.AddPageRoute("/Paths/Path", "/Paths/{name}");
+        options.Conventions.AddPageRoute("/Search", "/Search/{SearchString?}");
+    });
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+builder.Services.AddSignalR();
+builder.Services.AddApplicationInsightsTelemetry();
+
+builder.Services.AddSingleton<IOpenAIResponder, OpenAIResponder>();
+builder.Services.Configure<OpenAISettings>(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapRazorComponents<BiblePathsCore.App>()
+    .AddInteractiveServerRenderMode();
+app.MapHub<GameTeamHub>("/GameTeamHub");
+
+app.Run();
